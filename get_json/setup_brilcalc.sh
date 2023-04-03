34133b323ca299205bce05ac55663b1d72de3c89
Aram <aram.hayrapetyan@cern.ch>
2023-04-03 17:37:22 +0200

    Jobs for runs and lumi and brilcalc

---
+++ get_json/setup_brilcalc.sh
@@ -0,0 +1,6 @@
+cmsenv
+
+export PATH=$HOME/.local/bin:/cvmfs/cms-bril.cern.ch/brilconda/bin:$PATH
+export PATH=$HOME/.local/bin:/cvmfs/cms-bril.cern.ch/brilconda3/bin:$PATH
+export PATH=$HOME/.local/bin:/nfshome0/lumipro/brilconda/bin:$PATH
+export PATH=$HOME/.local/bin:/nfshome0/lumipro/brilconda3/bin:$PATH
